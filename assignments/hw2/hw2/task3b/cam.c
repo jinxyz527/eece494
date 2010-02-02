d5a9efa4e7ac5e0f8ea9890a7ec10257fe1deb24
Jason-Linux <git@jasonpoon.ca>
2010-02-02 05:18:46 -0800

    implementation of a hash table to store ip address and key. if collision detected, prepended to linked list

--- assignments/hw2/hw2/task3b/cam.c
+++ assignments/hw2/hw2/task3b/cam.c
@@ -14,35 +14,104 @@
 
 #include "defs.h"
 
-/* Note that you probably want to have a data structure
-   that is accessable to all routines in this file.  To do
-   this, you probably would define some structure (perhaps
-   called cam_t in the file cam.h.  Then you could 
-   create a variable of this type here by using:
+#define HASH_TABLE_SIZE 40000
 
-   cam_t cam;
+hashtable_t *pHashTbl;
 
-   This will create  a variable called cam (of type cam_h)
-   that can be accessed by any routine in this file.  */
+unsigned long crc(const void *key, const unsigned long len)
+{
+    unsigned int i;
+    unsigned long hash;
+    const unsigned char *k = key;
+    for (hash=len, i=0; i<len; ++i)
+        hash = (hash >> 8) ^ crcTable[(hash & 0xff) ^ k[i]];
+    return hash;
+}
 
 
 void cam_init()
 {
-   /* Write your code here */
-}
+    pHashTbl = malloc(sizeof(hashtable_t));
+    if (!pHashTbl) {
+        exit(1);
+    }
 
+    pHashTbl->nodes = calloc(HASH_TABLE_SIZE, sizeof(hashnode_t*));
+    if (!pHashTbl->nodes) {
+        free(pHashTbl);
+        exit(1);
+    }
+
+    pHashTbl->size = HASH_TABLE_SIZE;
+    pHashTbl->hashFunc = crc;
+}
 
 void cam_add_entry(ip_address_t *address, int port)
 {
-   /* Write your code here */
+    unsigned long index;
+    hashnode_t *pNode, *newNode;
+
+    index = pHashTbl->hashFunc(address, sizeof(ip_address_t)) % pHashTbl->size;
+    pNode = pHashTbl->nodes[index];
+    while (pNode) {
+//        ip_address_print(pNode->address);
+        if (!memcmp(pNode->address, address, sizeof(ip_address_t))) {
+            // update value
+            pNode->port = port;
+            return;
+        }
+        pNode = pNode->pNext;
+    }
+
+    newNode = malloc(sizeof(hashnode_t));
+    if (!newNode) {
+        cam_free();
+        exit(1);
+    }
+
+    newNode->address = malloc(sizeof(ip_address_t));
+    if (!newNode) {
+        free(newNode);
+        cam_free();
+        exit(1);
+    }
+
+    // add new node to beginning of linked list
+    ip_address_copy( address, newNode->address );
+    newNode->port = port;
+    newNode->pNext = pHashTbl->nodes[index];
+    pHashTbl->nodes[index] = newNode;
 }
 
 int cam_lookup_address(ip_address_t *address)
 {
-   /* Write your code here */
+    unsigned long index;
+    hashnode_t *pNode;
+
+    index = pHashTbl->hashFunc(address, sizeof(ip_address_t)) % pHashTbl->size;
+    pNode = pHashTbl->nodes[index];
+    while (pNode) {
+        if (!memcmp(pNode->address, address, sizeof(ip_address_t))) {
+            return pNode->port;
+        }
+        pNode = pNode->pNext;
+    }
+    return -1;
 }
 
 void cam_free()
 {
-   /* Write your code here */
+    int i;
+    hashnode_t *node, *next;
+
+    for (i = 0; i < pHashTbl->size; i++) {
+        node = pHashTbl->nodes[i];
+        while( node ) {
+            next = node->pNext;
+            free(node);
+            node = next;
+        }
+    }
+    free(pHashTbl->nodes);
+    free(pHashTbl);
 }
