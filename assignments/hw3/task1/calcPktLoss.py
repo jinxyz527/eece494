d15244ba276e25476a45d16351fd108a802bd0dd
Jason-Linux <git@jasonpoon.ca>
2010-03-14 03:26:48 -0700

    changing behavior of overall task. calculating pkt loss and avg rssi at a constant location

---
+++ assignments/hw3/task1/calcPktLoss.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from __future__ import division
+import os
+
+dir = "indoors"
+
+packet_start = 1
+packet_total = 25
+
+for file in os.listdir(dir):
+    num_packets = 0
+    rssi_total = 0
+    for line in open(dir + "/" + file, "r"):
+        vals = line.split()
+
+        if len(vals) <= 2:
+            continue
+
+        rssi = int(vals[1].strip("rssi="))
+        seq = int(vals[2].strip("data="))
+
+        if seq == 0:
+            # ignore
+            continue
+
+        rssi_total += rssi
+        num_packets += 1
+
+    print "%s \t-- avg rssi= %0.2f \tpkt loss= %0.2f" % (file, rssi_total/num_packets, (packet_total-num_packets)/packet_total*100)
