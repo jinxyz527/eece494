5f7878bbb37da2d813aae5e08d6fcf578cf14a0b
Jason-Linux <git@jasonpoon.ca>
2010-03-16 08:47:13 -0700

    Fixed exception thrown when script attempts to parse a file that is NOT a log that lies in the same directory

--- assignments/hw3/task1/calcPktLoss.py
+++ assignments/hw3/task1/calcPktLoss.py
@@ -3,28 +3,31 @@
 from __future__ import division
 import os
 
-dir = "indoors"
+dir = "kaiser"
 
 packet_start = 1
 packet_total = 25
 
-for file in sorted(os.listdir(dir)):
-    num_packets = 0
-    rssi_total = 0
-    for line in open(dir + "/" + file, "r"):
-        vals = line.split()
+try:
+    for file in sorted(os.listdir(dir)):
+        num_packets = 0
+        rssi_total = 0
+        for line in open(dir + "/" + file, "r"):
+            vals = line.split()
 
-        if len(vals) <= 2:
-            continue
+            if len(vals) <= 2:
+                continue
 
-        rssi = int(vals[1].strip("rssi="))
-        seq = int(vals[2].strip("data="))
+            rssi = int(vals[1].strip("rssi="))
+            seq = int(vals[2].strip("data="))
 
-        if seq == 0:
-            # ignore
-            continue
+            if seq == 0:
+                # ignore
+                continue
 
-        rssi_total += rssi
-        num_packets += 1
+            rssi_total += rssi
+            num_packets += 1
 
-    print "%s \t-- avg rssi= %0.2f \tpkt loss= %0.2f" % (file, rssi_total/num_packets, (packet_total-num_packets)/packet_total*100)
+        print "%s \t-- avg rssi= %0.2f \tpkt loss= %0.2f" % (file, rssi_total/num_packets, (packet_total-num_packets)/packet_total*100)
+except:
+    pass
